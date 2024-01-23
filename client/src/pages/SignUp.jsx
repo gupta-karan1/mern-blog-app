6eff49798e0a7406a236d3bdb3beda8c4a89af39
Karan Gupta <d22124440@mytudublin.ie>
2024-01-23 18:18:33 +0000

    Update SignUp page with form validation and error handling

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,66 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { HiMail, HiUser, HiLockClosed } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import {
+  HiMail,
+  HiUser,
+  HiLockClosed,
+  HiExclamationCircle,
+} from "react-icons/hi";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.username || !formData.password)
+      return setErrorMessage("Please fill in all fields");
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        navigate("/sign-in");
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 place-content-center p-10 md:p-20 bg-gradient-to-b from-indigo-500   to-pink-400 ">
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 place-content-center p-10 md:p-20 bg-gradient-to-b from-indigo-500 to-pink-400 ">
       {/* left */}
       <div>
         <h2 className="text-6xl xl:text-9xl font-black  text-white mb-10">
@@ -18,7 +74,7 @@ function SignUp() {
 
       {/* right */}
       <div>
-        <form className="flex flex-col gap-5">
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
           <h2 className="text-3xl font-semibold text-white">
             Sign Up to Get Started
           </h2>
@@ -35,8 +91,8 @@ function SignUp() {
               type="email"
               icon={HiMail}
               placeholder="john@email.com"
-              required
               sizing="lg"
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -51,8 +107,8 @@ function SignUp() {
               type="text"
               icon={HiUser}
               placeholder="johnDoe123"
-              required
               sizing="lg"
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -66,8 +122,8 @@ function SignUp() {
               id="password"
               type="password"
               icon={HiLockClosed}
-              required
               sizing="lg"
+              onChange={handleChange}
             />
           </div>
 
@@ -76,8 +132,16 @@ function SignUp() {
             gradientMonochrome={"purple"}
             pill
             className="drop-shadow-md"
+            disabled={loading}
           >
-            Sign Up
+            {loading ? (
+              <>
+                <Spinner color="white" size="sm" />
+                <span className="ml-2">Signing Up...</span>
+              </>
+            ) : (
+              "Sign Up"
+            )}
           </Button>
         </form>
 
@@ -90,6 +154,12 @@ function SignUp() {
             Sign In
           </Link>
         </div>
+
+        {errorMessage && (
+          <Alert className="mt-5" color="failure" icon={HiExclamationCircle}>
+            {errorMessage}
+          </Alert>
+        )}
       </div>
     </div>
   );
