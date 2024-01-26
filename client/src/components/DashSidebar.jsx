f45ba0a886fd3c9dee7555b2e748603cdf4bd148
Karan Gupta <d22124440@mytudublin.ie>
2024-01-26 23:00:40 +0000

    Add create post option to DashSidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -4,10 +4,12 @@ import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signoutSuccess } from "../../redux/user/userSlice.js";
+import { useSelector } from "react-redux";
 
 function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState("");
+  const { currentUser } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
 
@@ -45,6 +47,17 @@ function DashSidebar() {
     <Sidebar aria-label="Dashboard sidebar" className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
+          {currentUser.isAdmin && (
+            <Link to="/create-post">
+              <Sidebar.Item
+                active={tab === "create-post"}
+                as="div"
+                className="bg-gradient-to-r hover:from-blue-400 hover:to-purple-500 text-white from-pink-500 to-purple-500 rounded-full px-4 py-2 cursor-pointer mb-2"
+              >
+                Create a New Post
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
